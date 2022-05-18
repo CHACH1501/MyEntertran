d1e990eb6d465f787851bfff01161d1dd0fef11d
CHACH01 <6405001501@rumail.ru.ac.th>
2022-05-18 17:48:03 +0700

    :)

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,24 +1,24 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life",
-                        "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=wmiIUN-7qhE")
+Soul_Land = media.Movie("Soul Land",
+                        "Tang San spent his life in the Tang Outer Sect, dedicated to the creation and mastery of hidden weapons. Once he stole the secret lore of the Inner Sect to reach the pinnacle of his art, his only way out was death. But after throwing himself off the deadly Hell’s Peak he was reborn in a different world, the world of Douluo Dalu, a world where every person has a spirit of their own, and those with powerful spirits can practice their spirit power to rise and become Spirit Masters",
+                        "https://static.anislime.com/pexels/image/wNgoJ.webp",
+                        "https://www.youtube.com/watch?v=7TfetAbO38c")
 
 #print(toy_story.storyline)
 
-school_of_rock = media.Movie("School of Rock", 
-                             "Using rock music to learn",
-                             "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/School_of_Rock_Poster.jpg/220px-School_of_Rock_Poster.jpg",
-                             "https://www.youtube.com/watch?v=TExoc0MG4I4")
+Gangubai = media.Movie("Gangubai",
+                             "etter known as Gangubai Kothewali[b] or Gangubai Kathiawadi,[c] was an Indian social activist, prostitute and madam of a brothel in the Kamathipura area of Mumbai during the 1960s. Gangubai did a lot of work for sex-workers and for the well-being of orphans.[4] She gradually ended up operating her own brothel and is known to also have lobbied for the rights of commercial sex workers.[5]",
+                             "https://i0.wp.com/www.9tana.com/iupload/16523692193232022-05-12_22-26-43.jpg?resize=620%2C305",
+                             "https://www.youtube.com/watch?v=_dpjGTby2uo")
 
-ratatouille = media.Movie("Ratatouille",
-                          "A rat is a chef in Paris",
-                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                          "https://www.youtube.com/watch?v=NgsQ8mVkN8w")
+Throne_seal = media.Movie ("throne_seal",
+                          "6000 years ago, the Demon God Emperor, Feng Xiu and the 72 Demon God Pillars descended from the sky, all creatures were contaminated with the breath of the Demon God Pillar, and instantly mutated into demon creatures and then humans entered the dark age. After which, the human practitioners arranged from the six Temples stood up to block the progress of the demons, and gradually formed a situation d where humans and demons coexisted. Protagonist Long Haochen joins forces with the Knight Temple, one of the six great temples, to become a knight to save his mother. In the adventure of step-by-step growth, the adventures, tricks, and the destined love and friendship continue to be revealed. He, Long Haochen embraced the spirit of the knight and won the recognition of others through his personality and hard work. He first formed the No. 1 Demon Hunting Group with other talented teenagers in the Six Temples to fight demons, fighting for human survival and dignity. At the same time, he sacrificed his life to protect his partner and most precious lover. And the situation in the world changed unexpectedly, more conspiracies were going on, and deeper secrets were waiting for him to be revealed. In the end, whether Long Haochen can win the approval of the Divine Throne and rise to the highest honor in the Knight Temple, and whether he can face the moment when all the truth is revealed, and solve the biggest crisis in the whole world. , everything still has to be disclosed.",
+                          "https://i3.wp.com/animexin.xyz/wp-content/uploads/2022/04/throne-seal.jpg?resize=247,350",
+                          "https://www.youtube.com/watch?v=Bz8LZAuyAbM")
 
 
 
-movies = [toy_story, ratatouille, school_of_rock]
+movies = [Soul_Land, Gangubai,Throne_seal]
 fresh_tomatoes.open_movies_page(movies)
